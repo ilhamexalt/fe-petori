8361613bcf10cb523773866afaacf756db3816dc
Ilham Maulana W <wmeluck24@gmail.com>
2024-04-19 10:22:16 +0700

    fix bug

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -50,72 +50,54 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <div className="area">
-        <ul className="circles">
-          <div className="flex justify-center items-center w-full min-h-screen  ">
-            <div className="bg-white w-80 md:w-[400px] shadow-md hover:shadow-lg rounded-br-2xl rounded-bl-sm rounded-tl-2xl ">
-              <div className="w-full py-3 bg-gradient-to-r from-indigo-500 to-blue-500 rounded-br-2xl rounded-bl-sm rounded-tl-2xl rounded-tr-sm">
-                <div className="font-bold text-5xl flex justify-center">
-                  <img
-                    src={Petori}
-                    alt="Petori Logo "
-                    className="w-24 md:w-44"
-                  />
-                </div>
-                <p className="text-center text-xs font-light text-white">
-                  © 2024 - Development
-                </p>
+      <div className="flex justify-center items-center w-full min-h-screen bg-gray-50 ">
+        <div className="bg-white w-80 md:w-[400px] shadow-md hover:shadow-lg rounded-br-2xl rounded-bl-sm rounded-tl-2xl ">
+          <div className="w-full py-3 bg-gradient-to-r from-indigo-500 to-blue-500 rounded-br-2xl rounded-bl-sm rounded-tl-2xl rounded-tr-sm">
+            <div className="font-bold text-5xl flex justify-center">
+              <img src={Petori} alt="Petori Logo " className="w-24 md:w-44" />
+            </div>
+            <p className="text-center text-xs font-light text-white">
+              © 2024 - Development
+            </p>
+          </div>
+          <div className="pt-5 px-5 py-5">
+            <form
+              name="basic"
+              className="relative"
+              onSubmit={handleChangePassword}
+            >
+              <div className="relative">
+                <InputComponent
+                  id="phoneNumber"
+                  value={phoneNumber}
+                  type="number"
+                  className={
+                    error && !phoneNumber ? "mb-5 border-red-500" : "mb-5"
+                  }
+                  placeholder="Phone Number"
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
               </div>
-              <div className="pt-5 px-5 py-5">
-                <form
-                  name="basic"
-                  className="relative"
-                  onSubmit={handleChangePassword}
-                >
-                  <div className="relative">
-                    <InputComponent
-                      id="phoneNumber"
-                      value={phoneNumber}
-                      type="number"
-                      className={
-                        error && !phoneNumber ? "mb-5 border-red-500" : "mb-5"
-                      }
-                      placeholder="Phone Number"
-                      onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
-                  </div>
 
-                  <ButtonComponent
-                    disabled={loading}
-                    type="submit"
-                    className="w-full tracking-widest p-1 mb-2"
-                  >
-                    {loading ? <LoadingOutlined /> : "Reset"}
-                  </ButtonComponent>
-                </form>
+              <ButtonComponent
+                disabled={loading}
+                type="submit"
+                className="w-full tracking-widest p-1 mb-2"
+              >
+                {loading ? <LoadingOutlined /> : "Reset"}
+              </ButtonComponent>
+            </form>
 
-                <div className="text-center">
-                  <Link
-                    to="/"
-                    className="text-gray-800  hover:text-indigo-500 transition ease-in-out delay-75 text-xs md:text-sm  mb-2"
-                  >
-                    {"Back to sign in"}
-                  </Link>
-                </div>
-              </div>
+            <div className="text-center">
+              <Link
+                to="/"
+                className="text-gray-800  hover:text-indigo-500 transition ease-in-out delay-75 text-xs md:text-sm  mb-2"
+              >
+                {"Back to sign in"}
+              </Link>
             </div>
           </div>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+        </div>
       </div>
     </>
   );
